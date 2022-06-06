2b452798823a4ef753eb0377211df96d3dcfb6b6
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-06 19:12:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()+1);
+let num3=19;
+let num4=5;
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,8,5,15,17,19,20);
+console.log(mod)
